32ba34211d49b5ac0a1f66ef09ae2d51fdfc1896
Adam Brown <downtownadambrown@gmail.com>
2018-10-27 22:53:38 -0400

    Event listener added, page loads worked out, routing intact....time to launch, baby

--- app/public/js/survey.js
+++ app/public/js/survey.js
@@ -1,16 +1,13 @@
-const submitSurvey = function(){
-    const surveyAnswers = [];
-    console.log('lel');
-    for (let i = 1; i <= 10; i++){
-        const questionValue = $(`#q${i}`).val();
-        surveyAnswers.push(questionValue);
-    }
+const submitSurvey = function(e){
+    e.preventDefault();
+    console.log($(this).serialize());
 
-    console.log(surveyAnswers);
     /* $.ajax({
         url: "",
 
     }) */
 };
 
-$('#submit').on('click', submitSurvey);
+$(document).ready(() => {
+  $("form").on("submit", submitSurvey);
+});
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -9,18 +9,19 @@ const path = require('path');
 
 module.exports = function(app) {
   app.get('/survey', function(req, res) {
-    /* res.set('Content-Type', 'text/html; charset=utf-8'); */
+    res.set('Content-Type', 'text/html; charset=utf-8');
     res.sendFile(path.join(__dirname, '../public/survey.html'));
-  });
-
-  // If no matching route is found default to home
-  app.get('*', function(req, res) {
-    /* res.set('Content-Type', 'text/html; charset=utf-8'); */
-    res.sendFile(path.join(__dirname, '../public/home.html'));
   });
 
   app.get('/js/survey.js', function(req, res) {
     res.set('Content-Type', 'application/javascript; charset=utf-8'); 
     res.sendFile(path.join(__dirname, '../public/js/survey.js'));
   });
+
+  // If no matching route is found default to home
+  app.get('*', function(req, res) {
+    res.set('Content-Type', 'text/html; charset=utf-8');
+    res.sendFile(path.join(__dirname, '../public/home.html'));
+  });
+
 };
