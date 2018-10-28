1db20349a48cbda82d9cea4b92e2c432244872e8
Adam Brown <downtownadambrown@gmail.com>
2018-10-27 22:25:24 -0400

    Fixing bug

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -9,11 +9,18 @@ const path = require('path');
 
 module.exports = function(app) {
   app.get('/survey', function(req, res) {
+    /* res.set('Content-Type', 'text/html; charset=utf-8'); */
     res.sendFile(path.join(__dirname, '../public/survey.html'));
   });
 
   // If no matching route is found default to home
   app.get('*', function(req, res) {
+    /* res.set('Content-Type', 'text/html; charset=utf-8'); */
     res.sendFile(path.join(__dirname, '../public/home.html'));
+  });
+
+  app.get('/js/survey.js', function(req, res) {
+    res.set('Content-Type', 'application/javascript; charset=utf-8'); 
+    res.sendFile(path.join(__dirname, '../public/js/survey.js'));
   });
 };
